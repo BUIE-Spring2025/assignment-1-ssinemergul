c3ab2c9fe93dd8d11fc989528f9ec7c22a9b6126
ssinemergul <ssinemergul@gmail.com>
2025-03-09 16:11:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def int_to_roman(num):
-    """
-    Convert an integer to a Roman numeral.
-
-    :param num: Integer value between 1 and 3999 inclusive.
-    :return: A string representing the Roman numeral of the integer.
-    """
+    if  1<= num <=3999:
+        symbols=['M', 'D', 'C', 'L', 'X', 'V', 'I']
+        values= [1000, 500, 100, 50, 10,5, 1]
+        roman= ''
+        for i in range(len(values)):
+            while num >= values[i]:
+                roman +=symbols[i]
+                num -= symbols[i]
+        return roman 
+    else:
+        print('value must between 1 and 3999')
